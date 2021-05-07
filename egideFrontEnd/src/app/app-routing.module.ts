bc777f7ced2dac22be870da9870f77b278e8a85a
[Bruno] <[bruno.micalli@uol.com.br]>
2021-05-07 08:54:20 -0300

    Rotas feitas

--- egideFrontEnd/src/app/app-routing.module.ts
+++ egideFrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { LinhaTempoComponent } from './linha-tempo/linha-tempo.component';
+import { LogarComponent } from './logar/logar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'',redirectTo:'inicio',pathMatch:'full'},
+
+  {path:'logar',component:LogarComponent},
+  {path:'cadastrar',component:CadastrarComponent},
+  {path:'inicio',component:InicioComponent},
+  {path:'linha-tempo',component:LinhaTempoComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
